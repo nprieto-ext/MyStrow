45bf6808491acd09c992088221cf85de810bf633
Nicolas Prieto <nprieto-ext@tuifrance.com>
2026-03-12 08:03:26 -0400

    Release 3.0.23

--- core.py
+++ core.py
@@ -31,7 +31,7 @@ MEDIA_EXTENSIONS_FILTER = "Medias (*.mp3 *.wav *.flac *.aac *.ogg *.m4a *.wma *.
 
 # === CONFIGURATION GLOBALE ===
 APP_NAME = "MyStrow"
-VERSION = "3.0.22"
+VERSION = "3.0.23"
 
 # === FIREBASE (cles dans firebase_config.py, non versionne) ===
 try:
--- installer/maestro.iss
+++ installer/maestro.iss
@@ -1,6 +1,6 @@
 [Setup]
 AppName=MyStrow
-AppVersion=3.0.22
+AppVersion=3.0.23
 AppPublisher=MyStrow
 AppPublisherURL=https://mystrow.fr
 DefaultDirName={autopf}\MyStrow
