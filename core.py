ea1b2892d38b27d90cb89e471e70efbf44162036
Nicolas Prieto <nprieto-ext@tuifrance.com>
2026-03-18 20:43:57 -0400

    Release 3.0.47

--- core.py
+++ core.py
@@ -35,7 +35,7 @@ MEDIA_EXTENSIONS_FILTER = "Medias (*.mp3 *.wav *.flac *.aac *.ogg *.m4a *.wma *.
 
 # === CONFIGURATION GLOBALE ===
 APP_NAME = "MyStrow"
-VERSION = "3.0.46"
+VERSION = "3.0.47"
 
 # === FIREBASE (cles dans firebase_config.py, non versionne) ===
 try:
